d175478dd007de691c431ee9973a07233d5bdeb4
bartmarkiewicz <39217312+bartmarkiewicz@users.noreply.github.com>
2024-05-06 16:20:38 +0100

    another lab

--- lab/36-mvc/src/test/java/accounts/web/AccountControllerTests.java
+++ lab/36-mvc/src/test/java/accounts/web/AccountControllerTests.java
@@ -26,9 +26,8 @@ public class AccountControllerTests {
 		controller = new AccountController(new StubAccountManager());
 	}
 
-	// TODO-07: Remove the @Disabled annotation, run the test, it should now pass.
+	// -07: Remove the @Disabled annotation, run the test, it should now pass.
 	@Test
-	@Disabled
 	public void testHandleListRequest() {
 		List<Account> accounts = controller.accountList();
 
@@ -42,15 +41,22 @@ public class AccountControllerTests {
 		assertEquals(expectedAccountNumber, account.getNumber());
 	}
 
-	// TODO-10a: Remove the @Disabled annotation, run the test, it should pass.
+	// -10a: Remove the @Disabled annotation, run the test, it should pass.
 	@Test
-	@Disabled
 	public void testHandleDetailsRequest() {
-		// TODO-09a: Implement test code which calls the accountDetails() method on the controller.
+		// -09a: Implement test code which calls the accountDetails() method on the controller.
 		// - It will take one parameter - use "expectedAccountId" defined above
 		// - It will return an Account
 
-		// TODO-09b: Define the following assertions:
+		var account = controller.accountDetails(expectedAccountId);
+
+		// Non-empty list containing the one and only test account
+		assertNotNull(account);
+		// Validate that account
+		assertEquals(expectedAccountId, (long) account.getEntityId());
+		assertEquals(expectedAccountNumber, account.getNumber());
+
+		// -09b: Define the following assertions:
 		// - The account is not null
 		// - The account id matches "expectedAccountId" defined above
 		// - The account number matches "expectedAccountNumber" defined above
